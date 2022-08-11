faca91c1e8563a24a8a91a48d75a470429de653b
Jake Mattson <dev.ioparameters@simplelogin.fr>
2022-08-10 23:48:36 -0500

    DiscordKt 0.23.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import java.util.*
+
 group = "me.moeszyslak"
-version = Versions.BOT
-description = "A simple discord bot that allows members to alert staff"
+version = "3.0.0"
+description = "A discord bot for members to alert staff"
 
 plugins {
     kotlin("jvm") version "1.7.10"
@@ -13,25 +15,27 @@ repositories {
 }
 
 dependencies {
-    implementation("me.jakejmattson:DiscordKt:${Versions.DISCORDKT}")
+    implementation("me.jakejmattson:DiscordKt:0.23.3")
 }
 
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
+
+        Properties().apply {
+            setProperty("name", project.name)
+            setProperty("description", project.description)
+            setProperty("version", version.toString())
+            setProperty("url", "https://github.com/the-programmers-hangout/Lighthouse")
+
+            store(file("src/main/resources/bot.properties").outputStream(), null)
+        }
     }
 
     shadowJar {
         archiveFileName.set("Lighthouse.jar")
         manifest {
-            attributes(
-                "Main-Class" to "me.moeszyslak.lighthouse.MainKt"
-            )
+            attributes("Main-Class" to "me.moeszyslak.lighthouse.MainKt")
         }
     }
-}
-
-object Versions {
-    const val BOT = "3.0.0"
-    const val DISCORDKT = "0.23.2"
 }
--- src/main/kotlin/me/moeszyslak/lighthouse/Main.kt
+++ src/main/kotlin/me/moeszyslak/lighthouse/Main.kt
@@ -4,6 +4,7 @@ import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.Permission
 import dev.kord.common.entity.Permissions
 import dev.kord.core.supplier.EntitySupplyStrategy
+import dev.kord.gateway.Intent
 import dev.kord.gateway.Intents
 import dev.kord.gateway.PrivilegedIntent
 import me.jakejmattson.discordkt.dsl.bot
@@ -12,14 +13,10 @@ import me.moeszyslak.lighthouse.dataclasses.Configuration
 import java.awt.Color
 import java.time.Instant
 
-private val startup = Instant.now()
-
 @PrivilegedIntent
 @KordPreview
-suspend fun main() {
+fun main() {
     val token = System.getenv("BOT_TOKEN") ?: null
-
-    require(token != null) { "Expected the bot token as an environment variable" }
 
     bot(token) {
         val configuration = data("config/config.json") { Configuration() }
@@ -30,25 +27,9 @@ suspend fun main() {
             mentionAsPrefix = true
             commandReaction = null
             theme = Color.MAGENTA
-            documentCommands = true
             entitySupplyStrategy = EntitySupplyStrategy.cacheWithRestFallback
-            intents = Intents.none
+            intents = Intents(Intent.MessageContent)
             defaultPermissions = Permissions(Permission.ManageMessages)
-        }
-
-        mentionEmbed {
-            val guildConfiguration = configuration[it.guild!!.id]
-
-            title = "Lighthouse 3.0.0"
-            description = "A multi-guild discord bot to log everything and everything you could ever want"
-            color = it.discord.configuration.theme
-            thumbnail(it.discord.kord.getSelf().pfpUrl)
-            addInlineField("Source", "[GitHub](https://github.com/the-programmers-hangout/Lighthouse)")
-            addInlineField("Ping", it.discord.kord.gateway.averagePing?.toString() ?: "Unknown")
-            addInlineField("Startup", TimeStamp.at(startup, TimeStyle.RELATIVE))
-            addInlineField("Channel", "<#${guildConfiguration?.alertChannel}>")
-            addInlineField("Role", "<@${guildConfiguration?.alertRole}>")
-            footer(it.discord.versions.toString())
         }
     }
 }
